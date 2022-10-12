6d68c15c274d8df8eac5944d27de03f422979a71
B1TC0R3 <github@b1tc0r3.com>
2022-10-12 15:55:40 +0200

    Config moved to b1tc0r3/Peronsal-Configuration (private), Remove obsolete files

--- C/helloworld/helloworld.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-const char* const message = "Hello World";
-
-
-int main() {
-	puts(message);
-}
--- bash/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Clean orphan packages
-sudo pacman -Rsn $(pacman -Qdtq)
-
-# Delete cache
-rm -rf ~/.cache/
-
-
