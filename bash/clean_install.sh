d3f9ef1789a282e5c0b038e22f1c659d98a901ad
B1TC0R3 <github@b1tc0r3.com>
2022-10-12 15:33:29 +0200

    Install script that automaticaly clears pacman and yay cache

---
+++ bash/clean_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "Installing '\e[32m$1\e[0m' with pacman..."
+sudo pacman -S $1 && yes | pacman -Sc && exit
+
+echo "Failed to install '$1' with pacman"
+echo -n "Install with yay? [y/N]"
+read install_with_yay
+
+if [ "$install_with_yay" = "y" ]; then
+    echo -e "Installing '\e[32m$1\e[0m' with yay..."
+    yay -S $1 && yes | yay -Sc && exit
+fi
