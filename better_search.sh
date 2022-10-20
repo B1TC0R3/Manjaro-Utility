fc0dc188a595c7b021028caf0910e88859bb7b6b
B1TC0R3 <github@b1tc0r3.com>
2022-10-20 12:11:46 +0200

    Fix formatting

--- better_search.sh
+++ better_search.sh
@@ -2,8 +2,8 @@
 
 APP=$1
 
-echo -e "\x1b[1m\1xb[32mPacman packages:\x1b[0m"
+echo -e "\x1b[1m\x1b[32mPacman packages:\x1b[0m"
 pacman -Ss $APP
 
-echo -e "\x1b[1m\1xb[32mYay packages:\x1b[0m"
+echo -e "\x1b[1m\x1b[32mYay packages:\x1b[0m"
 yay -Ss $APP
