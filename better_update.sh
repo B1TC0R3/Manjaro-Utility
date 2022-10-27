dcd3f31cdb2f8f4fbf9cac700037e5d6ba8eef07
B1TC0R3 <github@b1tc0r3.com>
2022-10-27 11:07:31 +0200

    Add update script

---
+++ better_update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+function print_green () {
+    echo -e "\x1b[32m\x1b[1m$1\x1b[0m"
+}
+
+print_green "Updating pacman packages..."
+sudo pacman -Syu
+
+print_green "Updating yay packages..."
+yay -Syu
+
+print_green "Clearing pacman cache..."
+yes | sudo pacman -Sc && print_green "Cache cleared (pacman)!"
+
+print_green "Clearing yay cache..."
+yes | yay -Sc && print_green "Cache cleared (yay)!"
+
+print_green "Update complete!"
+
+
