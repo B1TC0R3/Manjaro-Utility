fa08fbad3464a3d566ce4f9cc779d7a65434c047
Thomas <77125551+B1TC0R3@users.noreply.github.com>
2022-12-03 19:02:12 +0100

    Remove obsolete variable

--- better_uninstall.sh
+++ better_uninstall.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-APP=$1
-
 function get_similar () {
-    echo -e "Package '\x1b[32m$APP\x1b[0m' not found!\nSimilar packages:"
+    echo -e "Package '\x1b[32m$1\x1b[0m' not found!\nSimilar packages:"
     pacman -Q | grep $APP
     exit
 }
 
-echo -e "Uninstalling '\x1b[32m$APP\x1b[0m'..."
-sudo pacman -Rcns $APP || get_similar
+echo -e "Uninstalling '\x1b[32m$1\x1b[0m'..."
+sudo pacman -Rcns $1 || get_similar
 yes | sudo pacman -Sc
